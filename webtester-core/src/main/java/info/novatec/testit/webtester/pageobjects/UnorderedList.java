d00d0577ccb73658ab54709b0e5e8c6efb2708dd
Nico Paul <nicopaul49@googlemail.com>
2016-12-06 16:42:44 +0100

    Added UnorderedList PageObject

---
+++ webtester-core/src/main/java/info/novatec/testit/webtester/pageobjects/UnorderedList.java
@@ -0,0 +1,7 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import info.novatec.testit.webtester.api.annotations.Mapping;
+
+@Mapping(tag = "ul")
+public class UnorderedList extends GenericList {
+}

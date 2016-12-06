fee655a6469bb31b21c0687607eb2ab8a97c9578
Nico Paul <nicopaul49@googlemail.com>
2016-12-06 16:43:37 +0100

    Added OrderedList PageObject

---
+++ webtester-core/src/main/java/info/novatec/testit/webtester/pageobjects/OrderedList.java
@@ -0,0 +1,7 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import info.novatec.testit.webtester.api.annotations.Mapping;
+
+@Mapping(tag = "ol")
+public class OrderedList extends GenericList {
+}

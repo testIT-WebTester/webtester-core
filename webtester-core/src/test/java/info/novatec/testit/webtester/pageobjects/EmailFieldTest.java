bf7903b4d962b2806789f4024c662ed3ba1dfce6
Nico Paul <nicopaul49@googlemail.com>
2016-12-06 15:45:06 +0100

    Added EmailField and Tests

---
+++ webtester-core/src/test/java/info/novatec/testit/webtester/pageobjects/EmailFieldTest.java
@@ -0,0 +1,32 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import info.novatec.testit.webtester.AbstractPageObjectTest;
+import info.novatec.testit.webtester.api.exceptions.WrongElementClassException;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+public class EmailFieldTest extends AbstractPageObjectTest{
+
+    @InjectMocks
+    EmailField cut;
+
+    /*  Correctness of Class    */
+
+    @Test
+    public final void testCorrectnessOfClassForWebElement_inputTag_searchFieldType() {
+        stubWebElementTagAndType("input", "email");
+        cut.validate(webElement);
+    }
+
+    @Test (expected = WrongElementClassException.class)
+    public final void testCorrectnessOfClassForWebElement_nonInputTag() {
+        stubWebElementTagAndType("other", null);
+        cut.validate(webElement);
+    }
+
+    @Test (expected = WrongElementClassException.class)
+    public final void testCorrectnessOfClassForWebElement_nonEmailFieldType() {
+        stubWebElementTagAndType("input", "other");
+        cut.validate(webElement);
+    }
+}
---
+++ webtester-core/src/test/java/integration/pageobjects/EmailFieldIntegrationTest.java
@@ -0,0 +1,85 @@
+package integration.pageobjects;
+
+import info.novatec.testit.webtester.api.annotations.IdentifyUsing;
+import info.novatec.testit.webtester.pageobjects.EmailField;
+import info.novatec.testit.webtester.pageobjects.PageObject;
+import integration.AbstractWebTesterIntegrationTest;
+import org.apache.commons.lang.StringUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.annotation.PostConstruct;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EmailFieldIntegrationTest extends AbstractWebTesterIntegrationTest{
+    EmailFieldTestPage page;
+
+    @Before
+    public void initPage() {
+        page = getBrowser().create(EmailFieldTestPage.class);
+    }
+
+    @Override
+    protected String getHTMLFilePath() {
+        return "html/pageobjects/emailField.html";
+    }
+
+    /*  getting text    */
+
+    @Test
+    public final void testThatGettingTextWorksWithParentImplementation() {
+        assertThat(page.withValue.getText(), is("value@test"));
+    }
+
+    /*  appending text  */
+
+    @Test
+    public final void testThatAppendingOfTextWorksWithParentImplementation() {
+        EmailField element = page.withValue.appendText(".de");
+        assertThat(element.getText(), is("value@test.de"));
+    }
+
+    /*  setting text    */
+
+    @Test
+    public final void testThatSettingOfTextWorksWithParentImplementation() {
+        EmailField element = page.empty.setText("value@test.de");
+        assertThat(element.getText(), is("value@test.de"));
+    }
+
+    @Test
+    public final void testThatSettingTextWithValueWorksWithParentImplementation() {
+        EmailField element = page.withValue.setText("still value@test");
+        assertThat(element.getText(), is("still value@test"));
+    }
+
+
+    /*  clearing text   */
+
+    @Test
+    public final void testThatClearingOfTextWorksWithParentImplementation() {
+        EmailField element = page.withValue.clearText();
+        assertThat(element.getText(), is(""));
+    }
+
+    /*  utilities   */
+
+    public static class EmailFieldTestPage extends PageObject {
+        @IdentifyUsing("empty")
+        EmailField empty;
+
+        @IdentifyUsing("withValue")
+        EmailField withValue;
+
+        @PostConstruct
+        void checkStartingConditions(){
+            assertThat(empty.isVisible(), is(true));
+            assertThat(withValue.isVisible(), is(true));
+
+            assertThat(empty.getText(), is(StringUtils.EMPTY));
+            assertThat(withValue.getText(), is("value@test"));
+        }
+    }
+}

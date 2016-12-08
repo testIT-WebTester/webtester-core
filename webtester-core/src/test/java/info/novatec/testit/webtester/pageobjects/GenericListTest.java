ebe32d5ac03c1522c71301c330dffff6330105a5
Nico Paul <nicopaul49@googlemail.com>
2016-12-08 10:25:22 +0100

    Added GenericListTest

---
+++ webtester-core/src/test/java/info/novatec/testit/webtester/pageobjects/GenericListTest.java
@@ -0,0 +1,114 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.openqa.selenium.WebElement;
+
+import java.util.LinkedList;
+
+import sun.plugin.dom.exception.NoModificationAllowedException;
+
+import info.novatec.testit.webtester.AbstractPageObjectTest;
+import info.novatec.testit.webtester.api.exceptions.WrongElementClassException;
+
+
+public class GenericListTest extends AbstractPageObjectTest{
+    @Mock
+    ListItem item1;
+
+    @Mock
+    ListItem item2;
+
+    @Spy
+    java.util.List<ListItem> listItems = new LinkedList<ListItem>();
+
+    @InjectMocks
+    GenericList cut;
+
+    /*  empty   */
+
+    @Test
+    public void testThatEmptyStateCanBeRetrieved_empty() {
+        assertThat(cut.isEmpty(), is(true));
+    }
+
+    @Test
+    public void testThatEmptyStateCanBeRetrieved_containsItems() {
+        addBothListItems();
+        assertThat(cut.isEmpty(), is(false));
+    }
+
+    /*  number of items    */
+
+    @Test
+    public void testThatNumberOfItemsCanBeRetrieved_empty() {
+        assertThat(cut.getNumberOfItems(), is(0));
+    }
+
+    @Test
+    public void testThatNumberOfItemsCanBeRetrieved_containsItems() {
+        addBothListItems();
+        assertThat(cut.getNumberOfItems(), is(2));
+    }
+
+    /* get item */
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void testThatItemCanBeRetrievedOutOfList_Empty() {
+        assertThat(cut.getItem(0), is(item1));
+    }
+
+    @Test
+    public void testThatCorrectItemCanBeRetrievedOutOfList_containsItems() {
+        addBothListItems();
+        assertThat(cut.getItem(0), is(item1));
+        assertThat(cut.getItem(1), is(item2));
+    }
+
+    /*  get items     */
+
+    @Test
+    public void testThatItemsCanBeRetrievedAsList_containsItems() {
+        addBothListItems();
+        assertThat(cut.getItems(), contains(item1, item2));
+    }
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void testThatRetrievedItemListCanNotBeModified() {
+        addBothListItems();
+        cut.getItems().clear();
+    }
+
+    /*  correctness of class    */
+
+    @Test
+    public void testCorrectnessOfClassForWebElement_orderedList() {
+        stubWebElementTag("ol");
+        cut.validate(webElement);
+    }
+
+    @Test
+    public void testCorrectnessOfClassForWebElement_unorderedList() {
+        stubWebElementTag("ul");
+        cut.validate(webElement);
+    }
+
+    @Test (expected = WrongElementClassException.class)
+    public void testCorrectnessOfClassForWebElement_other() {
+        stubWebElementTag("other");
+        cut.validate(webElement);
+    }
+
+    /*  utilities    */
+
+    private void addBothListItems() {
+        listItems.add(item1);
+        listItems.add(item2);
+    }
+}
--- webtester-core/src/test/java/integration/pageobjects/GenericListIntegrationTest.java
+++ webtester-core/src/test/java/integration/pageobjects/GenericListIntegrationTest.java
@@ -1,14 +1,21 @@
 package integration.pageobjects;
 
 import info.novatec.testit.webtester.api.annotations.IdentifyUsing;
+import info.novatec.testit.webtester.api.browser.Browser;
 import info.novatec.testit.webtester.api.exceptions.WrongElementClassException;
 import info.novatec.testit.webtester.pageobjects.GenericList;
+import info.novatec.testit.webtester.pageobjects.List;
 import info.novatec.testit.webtester.pageobjects.PageObject;
 import integration.AbstractWebTesterIntegrationTest;
 import org.junit.Before;
 import org.junit.Test;
 
-
+/**
+ * These tests check aspects of the {@link List list} page object which can only
+ * be verified by using them on a live web-site with a real {@link Browser
+ * browser}. Some of them might also check if we are using Selenium features
+ * correctly.
+ */
 public class GenericListIntegrationTest extends AbstractWebTesterIntegrationTest {
 
     GenericListTestPage page;
@@ -26,17 +33,17 @@ public class GenericListIntegrationTest extends AbstractWebTesterIntegrationTest
     /*  validation of mapping   */
 
     @Test
-    public final void orderedListTypeIsValidMapping() {
+    public final void testValidationOfMapping_orderedList() {
         assertPageObjectCanBeInitialized(page.orderedList);
     }
 
     @Test
-    public final void unorderedListTypeIsValidMapping() {
+    public final void testValidationOfMapping_unorderedList() {
         assertPageObjectCanBeInitialized(page.unorderedList);
     }
 
     @Test (expected = WrongElementClassException.class)
-    public final void nonListTypeIsInvalidMapping() {
+    public final void testValidationOfMapping_noList() {
         assertPageObjectCanBeInitialized(page.noList);
     }
 

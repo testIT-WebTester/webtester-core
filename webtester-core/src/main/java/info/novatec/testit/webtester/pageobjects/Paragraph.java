98a20f0322221f5475807032d8a5c26e87e7bd09
Nico Paul <nicopaul49@googlemail.com>
2016-12-09 12:28:41 +0100

    Added Paragraph to PageObject

---
+++ webtester-core/src/main/java/info/novatec/testit/webtester/pageobjects/Paragraph.java
@@ -0,0 +1,8 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import info.novatec.testit.webtester.api.annotations.Mapping;
+
+
+@Mapping(tag = "p")
+public class Paragraph extends PageObject {
+}

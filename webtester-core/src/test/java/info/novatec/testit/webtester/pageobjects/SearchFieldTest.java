58f8efa40e7199321f520e4e6f7f5e794f024c7c
Nico Paul <nicopaul49@googlemail.com>
2016-12-20 15:03:58 +0100

    Added SearchField and SearchFieldTest

---
+++ webtester-core/src/test/java/info/novatec/testit/webtester/pageobjects/SearchFieldTest.java
@@ -0,0 +1,35 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import info.novatec.testit.webtester.AbstractPageObjectTest;
+import info.novatec.testit.webtester.api.exceptions.WrongElementClassException;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+
+public class SearchFieldTest extends AbstractPageObjectTest {
+
+    @InjectMocks
+    SearchField cut;
+
+    /* Correctness of Class */
+
+    @Test
+    public final void testCorrectnessOfClassForWebElement_inputTag_searchFieldType() {
+        stubWebElementTagAndType("input", "search");
+        cut.validate(webElement);
+    }
+
+    @Test (expected = WrongElementClassException.class)
+    public final void testCorrectnessOfClassForWebElement_nonInputTag() {
+        stubWebElementTagAndType("other", null);
+        cut.validate(webElement);
+    }
+
+    @Test (expected = WrongElementClassException.class)
+    public final void testCorrectnessOfClassForWebElement_nonSearchFieldType() {
+        stubWebElementTagAndType("input", "other");
+        cut.validate(webElement);
+    }
+
+
+}

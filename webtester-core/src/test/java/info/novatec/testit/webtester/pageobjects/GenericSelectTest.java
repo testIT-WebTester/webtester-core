e6882776e1af22cc042bc38eb72b0eb19347d8f6
Nico Paul <nicopaul49@googlemail.com>
2016-12-20 16:05:31 +0100

    Added GenericSelectTest

---
+++ webtester-core/src/test/java/info/novatec/testit/webtester/pageobjects/GenericSelectTest.java
@@ -0,0 +1,27 @@
+package info.novatec.testit.webtester.pageobjects;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
+import info.novatec.testit.webtester.AbstractPageObjectTest;
+import info.novatec.testit.webtester.api.exceptions.WrongElementClassException;
+
+
+public class GenericSelectTest extends AbstractPageObjectTest {
+
+    @InjectMocks
+    GenericSelect cut;
+
+    @Test
+    public void testCorrectnessOfClassForWebElement_selectTag() {
+        stubWebElementTag("select");
+        cut.validate(webElement);
+    }
+
+    @Test(expected = WrongElementClassException.class)
+    public void testCorrectnessOfClassForWebElement_otherTag() {
+        stubWebElementTag("other");
+        cut.validate(webElement);
+    }
+
+}

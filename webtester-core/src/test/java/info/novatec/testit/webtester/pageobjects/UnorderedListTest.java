45e862903d5ce3267a1289797c5429b95fb71ca8
Nico Paul <nicopaul49@googlemail.com>
2017-01-04 10:57:36 +0100

    Changed assertions for better understanding

--- webtester-core/src/test/java/info/novatec/testit/webtester/pageobjects/UnorderedListTest.java
+++ webtester-core/src/test/java/info/novatec/testit/webtester/pageobjects/UnorderedListTest.java
@@ -59,8 +59,8 @@ public class UnorderedListTest extends AbstractPageObjectTest{
     @Test
     public final void testThatItemCanBeRetrievedCorrectly() {
         addBothItems();
-        assertThat(cut.getItem(0).equals(item1), is(true));
-        assertThat(cut.getItem(1).equals(item2), is(true));
+        assertThat(cut.getItem(0), is(item1));
+        assertThat(cut.getItem(1), is(item2));
     }
 
     /* get items */

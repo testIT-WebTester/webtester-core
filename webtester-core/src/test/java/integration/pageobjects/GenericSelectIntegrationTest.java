e6dca95ec939eda20bff8b2e17c6eae7bbb1a8fa
Nico Paul <nicopaul49@googlemail.com>
2017-01-04 10:52:57 +0100

    Changed assertions for better understanding

--- webtester-core/src/test/java/integration/pageobjects/GenericSelectIntegrationTest.java
+++ webtester-core/src/test/java/integration/pageobjects/GenericSelectIntegrationTest.java
@@ -1,6 +1,7 @@
 package integration.pageobjects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 
 import java.util.List;
@@ -35,23 +36,23 @@ public class GenericSelectIntegrationTest extends AbstractWebTesterIntegrationTe
     @Test
     public final void testThatListWithOptionTextsIsReturnedCorrectly_singleSelect() {
         List<String> texts = page.singleSelect.getAllOptionTexts();
-        assertThat(texts.get(0).equals("one"), is(true));
-        assertThat(texts.get(1).equals("two"), is(true));
-        assertThat(texts.get(2).equals("three"), is(true));
+        assertThat(texts.get(0), is("one"));
+        assertThat(texts.get(1), is("two"));
+        assertThat(texts.get(2), is("three"));
     }
 
     @Test
     public final void testThatListWithOptionTextsIsReturnedCorrectly_multiSelect() {
         List<String> texts = page.multiSelect.getAllOptionTexts();
-        assertThat(texts.get(0).equals("one"), is(true));
-        assertThat(texts.get(1).equals("two"), is(true));
-        assertThat(texts.get(2).equals("three"), is(true));
+        assertThat(texts.get(0), is("one"));
+        assertThat(texts.get(1), is("two"));
+        assertThat(texts.get(2), is("three"));
     }
 
     @Test
     public final void testThatListWithOptionTextsIsReturnedCorrectly_emptySelect() {
         List<String> optionTexts = page.emptySelect.getAllOptionTexts();
-        assertThat(optionTexts.isEmpty(), is(true));
+        assertThat(optionTexts, is(empty()));
     }
 
     /* get all option values */
@@ -59,24 +60,24 @@ public class GenericSelectIntegrationTest extends AbstractWebTesterIntegrationTe
     @Test
     public final void testThatListWithOptionValuesIsReturnedCorrectly_singleSelect() {
         List<String> values = page.singleSelect.getAllOptionValues();
-        assertThat(values.get(0).equals("1"), is(true));
-        assertThat(values.get(1).equals("2"), is(true));
-        assertThat(values.get(2).equals("3"), is(true));
+        assertThat(values.get(0), is("1"));
+        assertThat(values.get(1), is("2"));
+        assertThat(values.get(2), is("3"));
     }
 
     @Test
     public final void testThatListWithOptionValuesIsReturnedCorrectly_multiSelect() {
         List<String> values = page.multiSelect.getAllOptionValues();
-        assertThat(values.get(0).equals("1"), is(true));
-        assertThat(values.get(1).equals("2"), is(true));
-        assertThat(values.get(2).equals("3"), is(true));
+        assertThat(values.get(0), is("1"));
+        assertThat(values.get(1), is("2"));
+        assertThat(values.get(2), is("3"));
     }
 
 
     @Test
     public final void testThatListWithOptionValuesIsReturnedCorrectly_emptySelect() {
         List<String> optionValues = page.emptySelect.getAllOptionValues();
-        assertThat(optionValues.isEmpty(), is(true));
+        assertThat(optionValues, is(empty()));
     }
 
     /* number of options */
@@ -84,13 +85,13 @@ public class GenericSelectIntegrationTest extends AbstractWebTesterIntegrationTe
     @Test
     public final void testThatCorrectNumberOfOptionsIsReturned_singelSelect() {
         Integer numberOfOptions = page.singleSelect.getNumberOfOptions();
-        assertThat(numberOfOptions.equals(3), is(true));
+        assertThat(numberOfOptions, is(3));
     }
 
     @Test
     public final void testThatCorrectNumberOfOptionsIsReturned_multiSelect() {
         Integer numberOfOptions = page.multiSelect.getNumberOfOptions();
-        assertThat(numberOfOptions.equals(3), is(true));
+        assertThat(numberOfOptions, is(3));
     }
 
     /* validation of mapping */
